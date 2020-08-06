765248efcc5096903036dc456011a5301d549cc7
Gogs <gogs@fake.local>
2020-08-06 19:04:02 +0300

    input from console

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите свое имя: ");
+            string name = Console.ReadLine();       // вводим имя
+            Console.WriteLine($"Привет {name}");    // выводим имя на консоль   
+            Console.ReadKey();
         }
     }
 }
