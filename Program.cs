459cf236ae51d46280a23cc8b78997683e013eb3
Gogs <gogs@fake.local>
2020-08-07 08:57:58 +0300

    Add vars

--- Program.cs
+++ Program.cs
@@ -10,10 +10,14 @@ namespace HelloWorld
        */
         static void Main(string[] args)
         {
-            Console.Write("Введите свое имя: ");
-            string name = Console.ReadLine();       // вводим имя
-            Console.WriteLine($"Привет {name}");    // выводим имя на консоль   
-            Console.ReadKey();
+            string name = "Tom";  // определяем переменную и инициализируем ее
+
+            Console.WriteLine(name);    // Tom
+
+            name = "Bob";       // меняем значение переменной
+            Console.WriteLine(name);    // Bob
+
+            Console.Read();
         }
     }
 }
